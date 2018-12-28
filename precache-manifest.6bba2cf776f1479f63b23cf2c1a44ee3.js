10095b2d9c1fd9127e0a0c59f1c14acebbea8bc7
annaVih <anna-kobzii@mail.ru>
2018-12-28 11:44:06 +0000

    Updates

---
+++ precache-manifest.6bba2cf776f1479f63b23cf2c1a44ee3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a84ce474cb252a1849",
+    "url": "/streams/static/js/runtime~main.44a84ce4.js"
+  },
+  {
+    "revision": "9508ec1e2d1355b87b86",
+    "url": "/streams/static/js/main.9508ec1e.chunk.js"
+  },
+  {
+    "revision": "e8b271d8a4eeacb5eedc",
+    "url": "/streams/static/js/1.e8b271d8.chunk.js"
+  },
+  {
+    "revision": "5b91c9f15547450f1056603263b262a1",
+    "url": "/streams/index.html"
+  }
+];
